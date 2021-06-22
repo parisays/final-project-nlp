ff32a768544a34c10d208e06a0359b2ffd309be4
Parisa Yalsavar <parisa.yalsavar@gmail.com>
2021-06-23 00:22:02 +0430

    Modify word2vec

--- src/word2vec/main.py
+++ src/word2vec/main.py
@@ -5,8 +5,7 @@ genre = [ 'Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Romance' ]
 for label in genre:
     print(label)
     os.system("cp ../../data/word2vec/{0}.txt utils/datasets/stanfordSentimentTreebank/datasetSentences.txt".format(label))
-    # break
     os.system("python run.py")
-    # os.mkdir(label)
-    os.system("mv saved_params_1000.npy ../../models/word2vec/{0}".format(label))
-    os.system("mv saved_state_1000.pickle ../../models/word2vec/{0}".format(label))
+    os.system("mv saved_params_1000.npy ../../models/word2vec/{0}.word2vec.npy".format(label))
+    os.system("mv saved_state_1000.pickle ../../models/word2vec/{0}.word2vec.pickle".format(label))
+
